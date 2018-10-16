3deab7445d55ac7274837898d4f91ed253fab9bf
Noppadon Sodram <knopsod@gmail.com>
2018-10-16 14:49:35 +0700

    Add link reference about cURLs

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ curl_setopt($ch,CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
 // curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Authorization': 'Token token=tb3vQwhAnJc2PNkusWvkgB4pA8wnVLpy7CXpEsz7jL')); 
 // curl_setopt($ch, CURLOPT_POSTFIELDS, '{ "client_url":"https://itgadget.co.th","client_rest_api_endpoint":"","client_transaction_id":"asdadwdasda321545453","bank_account":"John Doe","bank_no": "999-9-9999-9","bank_name":"kasikorn","bank_short_name":"KBANK","transfer_type":"ATM","amount":"2999","transferred_datetime": "2018-01-01 10:00:00","transfer_detail": "Some comment message from user","is_approved": "false"}'); 
+
+// https://stackoverflow.com/questions/2138527/php-curl-http-post-sample-code
 curl_setopt($ch, CURLOPT_POSTFIELDS, 
   http_build_query(
     array(
